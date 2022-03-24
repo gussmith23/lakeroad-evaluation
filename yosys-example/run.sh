8c8fdee813820548fb552cdf6bce87dd4d2b7e1f
Gus Smith <guscomps@gmail.com>
2022-03-23 20:40:02 -0700

    Expand example script so that it actually does checks

--- yosys-example/run.sh
+++ yosys-example/run.sh
@@ -1,1 +1,13 @@
-yosys -p 'synth_xilinx -family xcup' -o synth.v example.v
+set -e
+
+# TODO(@gussmith23) We need to run twice to produce LUTs, for some reason.
+yosys -p 'synth_xilinx -family xcup; synth_xilinx -family xcup' -o synth.v example.v | tee synth.log
+
+# Parses the number of LUT2s out of the results.
+NUM_LUTS=$(sed -nr "s/[[:space:]]*LUT2[[:space:]]*([0-9]+)[[:space:]]*/\1/p" synth.log)
+
+# NUM_LUTS should be one line long.
+if [ $(echo $NUM_LUTS | wc -l) -ne "1" ]; then exit 1; fi
+
+# There should be one LUT2.
+if [ $NUM_LUTS -ne "1" ]; then echo "wrong number of LUTs found"; exit 1; fi
