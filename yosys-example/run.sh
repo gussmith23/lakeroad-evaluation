40bb5c988e19b01499adfad0031c676aff23f88a
Gus Smith <guscomps@gmail.com>
2022-03-24 18:51:06 -0700

    Synth and pnr for ecp5

--- yosys-example/run.sh
+++ yosys-example/run.sh
@@ -11,3 +11,15 @@ if [ $(echo $NUM_LUTS | wc -l) -ne "1" ]; then exit 1; fi
 
 # There should be one LUT2.
 if [ $NUM_LUTS -ne "1" ]; then echo "wrong number of LUTs found"; exit 1; fi
+
+# Same thing, but now we synthesize for Lattice ECP5. We can also place and
+# route for ECP5 using nextpnr-ecp5.
+yosys -s synthesize-ecp5.ys | tee synthesize-ecp5.ys
+NUM_LUTS=$(sed -nr "s/[[:space:]]*LUT4[[:space:]]*([0-9]+)[[:space:]]*/\1/p" synth.log)
+if [ $(echo $NUM_LUTS | wc -l) -ne "1" ]; then exit 1; fi
+if [ $NUM_LUTS -ne "1" ]; then echo "wrong number of LUTs found"; exit 1; fi
+
+nextpnr-ecp5 --json synth-ecp5.json 2>&1 | tee nextpnr-ecp5.log
+NUM_LUTS=$(sed -nr "s/.*logic LUTs:[[:space:]]*([0-9]+)\/.*/\1/p" nextpnr-ecp5.log)
+if [ $(echo $NUM_LUTS | wc -l) -ne "1" ]; then exit 1; fi
+if [ $NUM_LUTS -ne "1" ]; then echo "wrong number of LUTs found"; exit 1; fi
