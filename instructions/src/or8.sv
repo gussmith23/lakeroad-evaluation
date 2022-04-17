c18b7093df4fbc878c90db2b334854653178a4bc
bkushigian <bkushigian@gmail.com>
2022-04-17 15:06:20 -0700

    Initial implementations for basic operations

--- instructions/src/or8.sv
+++ instructions/src/or8.sv
@@ -1,3 +1,3 @@
 module example(input [7:0] a, input [7:0] b, output [7:0] out);
-  assign out = a & b;
+  assign out = a | b;
 endmodule
--- instructions/src/shl16.sv
+++ instructions/src/shl16.sv
@@ -1,3 +1,3 @@
 module example(input [15:0] a, input [15:0] b, output [15:0] out);
-  assign out = a & b;
+  assign out = a << b;
 endmodule
