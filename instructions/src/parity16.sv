e3572448246f9576602b8daa37cbc817e2fe8ac9
bkushigian <bkushigian@gmail.com>
2022-04-21 17:16:25 -0700

    Manually implemented parity

---
+++ instructions/src/parity16.sv
@@ -0,0 +1,3 @@
+module example(input [15:0] a, output [0:0] out);
+  assign out = a[15];
+endmodule
---
+++ instructions/src/parity8.sv
@@ -0,0 +1,3 @@
+module example(input [7:0] a, output [0:0] out);
+  assign out = a[7];
+endmodule
