fcbfed1778b2e93da2ce3c408137014dabeff7c3
Gus Smith <guscomps@gmail.com>
2022-05-07 17:45:51 -0700

    Clean up where example writes files; build with Verilator

--- yosys-example/run.sh
+++ yosys-example/run.sh
@@ -1,16 +1,21 @@
 set -e
 
 # TODO(@gussmith23) We need to run twice to produce LUTs, for some reason.
-yosys -s synthesize-xilinx.ys | tee synthesize-xilinx.log
+mkdir -p synth-xilinx
+yosys -s synthesize-xilinx.ys | tee synth-xilinx/synth.log
 
 # Parses the number of LUT2s out of the results.
-NUM_LUTS=$(sed -nr "s/[[:space:]]*LUT2[[:space:]]*([0-9]+)[[:space:]]*/\1/p" synthesize-xilinx.log)
+NUM_LUTS=$(sed -nr "s/[[:space:]]*LUT2[[:space:]]*([0-9]+)[[:space:]]*/\1/p" synth-xilinx/synth.log)
 
 # NUM_LUTS should be one line long.
 if [ $(echo $NUM_LUTS | wc -l) -ne "1" ]; then exit 1; fi
 
 # There should be one LUT2.
 if [ $NUM_LUTS -ne "1" ]; then echo "wrong number of LUTs found"; exit 1; fi
+
+# Build with Verilator.
+# TODO(@gussmith23) Write an actual testbench.
+verilator  -Wno-TIMESCALEMOD --cc synth-xilinx/example.v -I ../verilator-unisims/LUT2.v
 
 # Same thing, but now we synthesize for Lattice ECP5. We can also place and
 # route for ECP5 using nextpnr-ecp5.
