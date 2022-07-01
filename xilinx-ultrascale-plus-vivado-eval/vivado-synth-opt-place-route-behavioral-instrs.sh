3289b095ecf4584562c51fc8289a74213708f0d3
Gus Smith <guscomps@gmail.com>
2022-07-01 16:14:46 +0000

    Exclude replicate and extract

--- xilinx-ultrascale-plus-vivado-eval/vivado-synth-opt-place-route-behavioral-instrs.sh
+++ xilinx-ultrascale-plus-vivado-eval/vivado-synth-opt-place-route-behavioral-instrs.sh
@@ -8,7 +8,8 @@ mkdir -p $EVAL_OUTPUT_DIR
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 BASE_DIR="$SCRIPT_DIR/../"
 
-instr_files=$(find $BASE_DIR/instructions/src/ -type f)
+# Exclude extract and replicate, as they are just wires.
+instr_files=$(find $BASE_DIR/instructions/src/ -type f \( ! -name "*extract*" ! -name "*replicate*" \))
 num_files=$(echo "$instr_files" | wc -l)
 i=0
 for instr_file in $instr_files ; do
