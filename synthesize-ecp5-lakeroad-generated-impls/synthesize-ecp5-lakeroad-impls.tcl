196eb14220d9cabf6be22956f764dcbd44d3655c
bkushigian <bkushigian@gmail.com>
2022-07-01 14:43:10 -0700

    Added lattice eval scripts

--- synthesize-ecp5-lakeroad-generated-impls/synthesize-ecp5-lakeroad-impls.tcl
+++ synthesize-ecp5-lakeroad-generated-impls/synthesize-ecp5-lakeroad-impls.tcl
@@ -1,2 +1,10 @@
-# See https://github.com/uwsampl/lakeroad-evaluation/blob/d0a83ac138a6db95da264a3cd001bc27ee928fa4/xilinx-ultrascale-plus-vivado-eval/vivado-synth-opt-place-route-instrs.tcl
-# for an example script to copy.
+set sv_source_file $::env(VLOG_FILE_NAME)
+set base $::env(OUTPUT_FILE_BASE)
+set modname $::env(MOD_NAME)
+
+yosys read_verilog -sv "$sv_source_file"
+yosys hierarchy -top "$modname"
+yosys synth_ecp5
+yosys write_verilog "$base-synth.v"
+yosys write_json "$base-synth.json"
+exit
