bbe6801f803e1c88fc3b9a0240f86f56f90d8188
Gus Smith <guscomps@gmail.com>
2022-07-06 23:12:09 +0000

    Clean up folder

--- luis-vector-add-not-using-carry-example/run.tcl
+++ luis-vector-add-not-using-carry-example/run.tcl
@@ -1,12 +1,18 @@
+if { $argc != 2 } {
+  error "<sv_source_file>, <output_files_filename_base>"
+}
+
+set sv_source_file [ lindex $argv 0 ]
+set base [ lindex $argv 1 ]
+
 create_project -force "add" "out" -part "xczu3eg-sbva484-1-e"
 
-read_verilog -sv lakeroad.v
+read_verilog -sv $sv_source_file
 read_xdc -mode "out_of_context" constraints.xdc
 
 synth_design -mode "out_of_context" -top "main"
-write_verilog "netlist.v"
+write_verilog "$base-netlist.v"
 place_design -directive Default
 route_design -directive Default
-report_utilization -file "util.txt"
-report_timing -file "timing.txt"
-report_timing_summary -check_timing_verbose -file "timing_sum.txt"
+report_utilization -file "$base-util.txt"
+report_timing -file "$base-timing.txt"
