3e1acf875aa0345b575e56dee6624e0856012871
Gus Smith <guscomps@gmail.com>
2022-07-06 22:30:41 +0000

    Add switch to skip tests

--- run.sh
+++ run.sh
@@ -7,42 +7,47 @@ set -u
 # Verbose/debug.
 set -x
 
-pushd lakeroad
-# Note: ALWAYS use source! This lets us fail if any commands in these scripts fail.
-source run-tests.sh
-popd
+# Run tests, unless we explicitly skip them.
+if [ ! -v SKIP_TESTS ]; then
 
-pushd yosys-example
-source run.sh
-popd
+  pushd lakeroad
+  # Note: ALWAYS use source! This lets us fail if any commands in these scripts fail.
+  source run-tests.sh
+  popd
 
-# Run Calyx tests. Note that a lot of the non-core tests won't work due to other
-# missing deps.
-runt -i core calyx/
-# Test compilation with Calyx.
-calyx/bin/fud e calyx/examples/tutorial/language-tutorial-iterate.futil \
-  -s verilog.data calyx/examples/tutorial/data.json \
-  --to dat --through verilog -v
-# Run Calyx core tests on UltraScale. We exclude two tests which fail only because of textual differences in core.sv.
-runt -d -x '(big-const.futil)|(memory-with-external-attribute.futil)' -i core calyx-xilinx-ultrascale-plus/
-calyx-xilinx-ultrascale-plus/bin/fud e calyx-xilinx-ultrascale-plus/examples/tutorial/language-tutorial-iterate.futil \
-  -s verilog.data calyx-xilinx-ultrascale-plus/examples/tutorial/data.json \
-  --to dat --through verilog -v
-# Run Calyx core tests on Lattice. We exclude two tests which fail only because of textual differences in core.sv.
-runt -d -x '(big-const.futil)|(memory-with-external-attribute.futil)' -i core calyx-lattice-ecp5/
-calyx-lattice-ecp5/bin/fud e calyx-lattice-ecp5/examples/tutorial/language-tutorial-iterate.futil \
-  -s verilog.data calyx-lattice-ecp5/examples/tutorial/data.json \
-  --to dat --through verilog -v
+  pushd yosys-example
+  source run.sh
+  popd
 
-# Run Calyx tests with Xilinx generated instruction impls.
-. calyx-xilinx-ultrascale-plus/bin/activate # Note: use . instead of source.
-runt -d -x '(relay)|(mrxl)|(ntt)|(dahlia)|(NTT)|(\[frontend\] dahlia)|(\[core\] backend)' calyx-xilinx-ultrascale-plus/
-deactivate
+  # Run Calyx tests. Note that a lot of the non-core tests won't work due to other
+  # missing deps.
+  runt -i core calyx/
+  # Test compilation with Calyx.
+  calyx/bin/fud e calyx/examples/tutorial/language-tutorial-iterate.futil \
+    -s verilog.data calyx/examples/tutorial/data.json \
+    --to dat --through verilog -v
+  # Run Calyx core tests on UltraScale. We exclude two tests which fail only because of textual differences in core.sv.
+  runt -d -x '(big-const.futil)|(memory-with-external-attribute.futil)' -i core calyx-xilinx-ultrascale-plus/
+  calyx-xilinx-ultrascale-plus/bin/fud e calyx-xilinx-ultrascale-plus/examples/tutorial/language-tutorial-iterate.futil \
+    -s verilog.data calyx-xilinx-ultrascale-plus/examples/tutorial/data.json \
+    --to dat --through verilog -v
+  # Run Calyx core tests on Lattice. We exclude two tests which fail only because of textual differences in core.sv.
+  runt -d -x '(big-const.futil)|(memory-with-external-attribute.futil)' -i core calyx-lattice-ecp5/
+  calyx-lattice-ecp5/bin/fud e calyx-lattice-ecp5/examples/tutorial/language-tutorial-iterate.futil \
+    -s verilog.data calyx-lattice-ecp5/examples/tutorial/data.json \
+    --to dat --through verilog -v
 
-# Run Calyx tests with Lattice generated instruction impls.
-. calyx-lattice-ecp5/bin/activate # Note: use . instead of source.
-runt -d -x '(relay)|(mrxl)|(ntt)|(dahlia)|(NTT)|(\[frontend\] dahlia)|(\[core\] backend)' calyx-lattice-ecp5/
-deactivate
+  # Run Calyx tests with Xilinx generated instruction impls.
+  . calyx-xilinx-ultrascale-plus/bin/activate # Note: use . instead of source.
+  runt -d -x '(relay)|(mrxl)|(ntt)|(dahlia)|(NTT)|(\[frontend\] dahlia)|(\[core\] backend)' calyx-xilinx-ultrascale-plus/
+  deactivate
+
+  # Run Calyx tests with Lattice generated instruction impls.
+  . calyx-lattice-ecp5/bin/activate # Note: use . instead of source.
+  runt -d -x '(relay)|(mrxl)|(ntt)|(dahlia)|(NTT)|(\[frontend\] dahlia)|(\[core\] backend)' calyx-lattice-ecp5/
+  deactivate
+
+fi
 
 # Generate Yosys
 pushd instructions
