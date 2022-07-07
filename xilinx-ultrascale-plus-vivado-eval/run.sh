7e8f540d33f2734c7d932fa6e0d0bc4c9310e5ad
Gus Smith <guscomps@gmail.com>
2022-07-07 17:30:16 +0000

    Fix bug

--- xilinx-ultrascale-plus-vivado-eval/run.sh
+++ xilinx-ultrascale-plus-vivado-eval/run.sh
@@ -10,4 +10,4 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 $SCRIPT_DIR/compare-fud-resource-estimates.sh
 $SCRIPT_DIR/vivado-synth-opt-place-route.sh
 $SCRIPT_DIR/vivado-synth-opt-place-route-lakeroad-ultrascale-instrs.sh
-$SCRIPT_DIR/vivado-synth-opt-place-route-lakeroad-behavioral-instrs.sh
+$SCRIPT_DIR/vivado-synth-opt-place-route-behavioral-instrs.sh
