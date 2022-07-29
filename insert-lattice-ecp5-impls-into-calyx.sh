c3d6abccb266abffe4505ef53423035de94fc3b9
Gus Smith <guscomps@gmail.com>
2022-07-29 21:29:51 +0000

    Fully replace generated LR code

--- insert-lattice-ecp5-impls-into-calyx.sh
+++ insert-lattice-ecp5-impls-into-calyx.sh
@@ -6,6 +6,6 @@ set -u
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 IMPLS_DIR="$SCRIPT_DIR/lattice_ecp5_impls"
 
-# Insert them into the file. Note that this doesn't delete any existing code, so
-# you might have to clean up old code.
-sed -i.orig '/^\/\/ BEGIN GENERATED LAKEROAD CODE$/r '<(cat $IMPLS_DIR/*) "$SCRIPT_DIR/calyx-lattice-ecp5/primitives/core.sv"
+# Clear old implementations, insert new ones into file.
+sed -i -n '1,/BEGIN GENERATED LAKEROAD CODE/p;/END GENERATED LAKEROAD CODE/,$p' "$SCRIPT_DIR/calyx-lattice-ecp5/primitives/core.sv"
+sed -i '/^\/\/ BEGIN GENERATED LAKEROAD CODE$/r '<(cat $IMPLS_DIR/*) "$SCRIPT_DIR/calyx-lattice-ecp5/primitives/core.sv"
