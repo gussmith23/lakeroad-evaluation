fb3d9bccd0043d557f2d7e3689c64f164873d19d
Gus Smith <guscomps@gmail.com>
2022-09-11 22:59:29 -0700

    Update instructions.yaml generation

--- python/utils.py
+++ python/utils.py
@@ -1,5 +1,15 @@
 from pathlib import Path
 
+
 def lakeroad_evaluation_dir() -> Path:
     """Return the path to the Lakeroad evaluation directory."""
     return Path(__file__).parent.parent.resolve()
+
+
+def make_module_name(
+    architecture: str, instruction_name: str, bitwidth: int, arity: int
+) -> str:
+    architecture_with_underscores = architecture.replace("-", "_")
+    return (
+        f"lakeroad_{architecture_with_underscores}_{instruction_name}{bitwidth}_{arity}"
+    )
