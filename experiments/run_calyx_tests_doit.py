f71e6c1135f5fc614e4e9c83cd340b3f7c275387
Gus Smith <guscomps@gmail.com>
2022-09-19 15:28:41 +0000

    Change path, remove dead code

--- experiments/run_calyx_tests_doit.py
+++ experiments/run_calyx_tests_doit.py
@@ -40,7 +40,7 @@ def _insert_instructions_and_run_tests(
             [
                 "bash",
                 "-c",
-                f"source {calyx_dir /'bin'/'activate'};runt -d -x '(relay)|(mrxl)|(ntt)|(dahlia)|(NTT)|(\[frontend\] dahlia)|(\[core\] backend)' {calyx_dir}",
+                f"source {calyx_dir /'venv' / 'bin'/'activate'};runt -d -x '(relay)|(mrxl)|(ntt)|(dahlia)|(NTT)|(\[frontend\] dahlia)|(\[core\] backend)' {calyx_dir}",
             ],
             stderr=f,
             stdout=f,
@@ -91,64 +91,3 @@ def task_run_calyx_tests():
             )
         ],
     }
-
-
-class RunCalyxTests(Experiment):
-    def __init__(
-        self,
-        lattice_ecp5_impls_dir,
-        lattice_ecp5_calyx_dir,
-        xilinx_ultrascale_plus_impls_dir,
-        xilinx_ultrascale_plus_calyx_dir,
-    ) -> None:
-        super().__init__()
-
-        self._lattice_ecp5_impls_dir = lattice_ecp5_impls_dir
-        self._lattice_ecp5_calyx_dir = lattice_ecp5_calyx_dir
-        self._xilinx_ultrascale_plus_impls_dir = xilinx_ultrascale_plus_impls_dir
-        self._xilinx_ultrascale_plus_calyx_dir = xilinx_ultrascale_plus_calyx_dir
-
-    def _run_experiment(self):
-        _insert_instructions_and_run_tests(
-            self._lattice_ecp5_calyx_dir, self._lattice_ecp5_impls_dir
-        )
-        _insert_instructions_and_run_tests(
-            self._xilinx_ultrascale_plus_calyx_dir,
-            self._xilinx_ultrascale_plus_impls_dir,
-        )
-
-
-if __name__ == "__main__":
-    import argparse
-
-    logging.basicConfig(level=os.environ.get("LOGLEVEL", "WARNING"))
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--lattice-ecp5-impls-dir",
-        help="Directory of Lattice ECP5 instruction implementations.",
-        required=True,
-        type=Path,
-    )
-    parser.add_argument(
-        "--lattice-ecp5-calyx-dir",
-        help="Directory of the Lattice ECP5 Calyx instance.",
-        required=True,
-        type=Path,
-    )
-    parser.add_argument(
-        "--xilinx-ultrascale-plus-impls-dir",
-        help="Directory of Xilinx UltraScale+ instruction implementations.",
-        required=True,
-        type=Path,
-    )
-    parser.add_argument(
-        "--xilinx-ultrascale-plus-calyx-dir",
-        help="Directory of the Xilinx UltraScale+ Calyx instance.",
-        required=True,
-        type=Path,
-    )
-    args = parser.parse_args()
-
-    # Construct and run the top-level experiment.
-    RunCalyxTests(**vars(args)).run()
