3a0fed7cf7715e518c9ebc3746cba35730ad654f
Gus Smith <guscomps@gmail.com>
2022-10-18 23:39:58 +0000

    Add baseline synthesis experiment

--- experiments/dodo.py
+++ experiments/dodo.py
@@ -2,3 +2,4 @@
 from generate_instruction_experiments import task_instruction_experiments
 from run_calyx_tests_doit import task_run_calyx_tests
 from calyx_end_to_end_doit import (task_xilinx_ultrascale_plus_calyx_end_to_end, task_lattice_ecp5_calyx_end_to_end, task_vanilla_calyx_end_to_end)
+from baseline_synthesis_doit import task_baseline_synthesis
