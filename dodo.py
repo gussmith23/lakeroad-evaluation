6df21d0b6fc3dac2978a5ca0cc8ae28426fc44a1
Gus Smith <guscomps@gmail.com>
2022-11-12 21:54:13 +0000

    Big cleanup

---
+++ dodo.py
@@ -0,0 +1,12 @@
+"""DoIt main file.
+
+The dodo.py file is like a Makefile for Python's DoIt library. Running `doit` in
+a directory with a dodo.py file (or passing the -f <filepath> option to point to
+a Python file) will run DoIt, loading the DoIt tasks starting from the dodo.py
+file. It is the job of the dodo.py file to import all tasks to make them visible
+to DoIt.
+"""
+
+from baseline_synthesis import task_baseline_synthesis
+from calyx import *
+from lakeroad import task_instruction_experiments
--- experiments/dodo.py
+++
@@ -1,7 +0,0 @@
-"""Main dodo file for DoIt. Imports all DoIt tasks so they are visible."""
-from generate_instruction_experiments import task_instruction_experiments
-
-# from run_calyx_tests_doit import task_run_calyx_tests
-# from calyx_end_to_end_doit import (task_xilinx_ultrascale_plus_calyx_end_to_end, task_lattice_ecp5_calyx_end_to_end, task_vanilla_calyx_end_to_end)
-from baseline_synthesis_doit import task_baseline_synthesis
-from calyx import *
