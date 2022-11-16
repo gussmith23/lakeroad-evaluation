5a82aa577aa37ff4928fe99e91eb94ce548db67e
Gus Smith <guscomps@gmail.com>
2022-11-16 20:15:39 +0000

    add more instructions

---
+++ instructions/src/std_mem_d1_1_1_5.sv
@@ -0,0 +1,36 @@
+module std_mem_d1_1_1_5 #(
+    parameter WIDTH = 1,
+    parameter SIZE = 1,
+    parameter IDX_SIZE = 5
+) (
+   input wire                logic [IDX_SIZE-1:0] addr0,
+   input wire                logic [ WIDTH-1:0] write_data,
+   input wire                logic write_en,
+   input wire                logic clk,
+   output logic [ WIDTH-1:0] read_data,
+   output logic              done
+);
+
+  (* keep = "true" *) logic [WIDTH-1:0] mem[SIZE-1:0];
+
+  /* verilator lint_off WIDTH */
+  assign read_data = mem[addr0];
+  always_ff @(posedge clk) begin
+    if (write_en) begin
+      mem[addr0] <= write_data;
+      done <= 1'd1;
+    end else done <= 1'd0;
+  end
+
+  // Check for out of bounds access
+  `ifdef VERILATOR
+    always_comb begin
+      if (addr0 >= SIZE)
+        $error(
+          "std_mem_d1: Out of bounds access\n",
+          "addr0: %0d\n", addr0,
+          "SIZE: %0d", SIZE
+        );
+    end
+  `endif
+endmodule
