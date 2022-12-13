7b01a0554c0314d7b98c7fb26ce17df0fda171d5
Gus Smith <guscomps@gmail.com>
2022-12-13 00:40:47 +0000

    Initial Lattice figure impl

--- python/figures_and_tables.py
+++ python/figures_and_tables.py
@@ -171,11 +171,25 @@ def task_make_sofa_figure(
 @doit.task_params(
     [
         {
-            "name": "gathered_data_filepath",
+            "name": "gathered_lakeroad_data_filepath",
             "default": str(
                 utils.output_dir()
                 / "gathered_data"
-                / "lakeroad_lattice_ecp5_results.csv"
+                / "lattice_ecp5_diamond_lakeroad_results.csv"
+            ),
+            "type": str,
+        },
+        {
+            "name": "gathered_diamond_data_filepath",
+            "default": str(
+                utils.output_dir() / "gathered_data" / "diamond_baseline.csv"
+            ),
+            "type": str,
+        },
+        {
+            "name": "gathered_yosys_data_filepath",
+            "default": str(
+                utils.output_dir() / "gathered_data" / "yosys_lattice_ecp5_baseline.csv"
             ),
             "type": str,
         },
@@ -197,12 +211,16 @@ def task_make_sofa_figure(
     ]
 )
 def task_make_lattice_ecp5_figure(
-    gathered_data_filepath: Union[str, Path],
+    gathered_lakeroad_data_filepath: Union[str, Path],
+    gathered_diamond_data_filepath: Union[str, Path],
+    gathered_yosys_data_filepath: Union[str, Path],
     csv_filepath: Union[str, Path],
     tex_filepath: Union[str, Path],
     excel_filepath: Union[str, Path],
 ):
-    gathered_data_filepath = Path(gathered_data_filepath)
+    gathered_lakeroad_data_filepath = Path(gathered_lakeroad_data_filepath)
+    gathered_diamond_data_filepath = Path(gathered_diamond_data_filepath)
+    gathered_yosys_data_filepath = Path(gathered_yosys_data_filepath)
     csv_filepath = Path(csv_filepath)
     tex_filepath = Path(tex_filepath)
     excel_filepath = Path(excel_filepath)
@@ -217,6 +235,7 @@ def task_make_lattice_ecp5_figure(
             ("Lattice ECP5", "Lakeroad", "MUL? (sec)"),
             ("Lattice ECP5", "Yosys", "Runtime in sec"),
             ("Lattice ECP5", "Yosys", "#LUTs"),
+            ("Lattice ECP5", "Yosys", "#muxes"),
             ("Lattice ECP5", "Yosys", "#CCU2Cs"),
             ("Lattice ECP5", "Yosys", "#MUL"),
             ("Lattice ECP5", "Diamond", "Runtime in sec"),
@@ -233,10 +252,165 @@ def task_make_lattice_ecp5_figure(
         tex_filepath.parent.mkdir(parents=True, exist_ok=True)
         excel_filepath.parent.mkdir(parents=True, exist_ok=True)
 
-        # data = pd.read_csv(gathered_data_filepath)
+        lakeroad_data = pd.read_csv(gathered_lakeroad_data_filepath)
+        diamond_data = pd.read_csv(gathered_diamond_data_filepath)
+        yosys_data = pd.read_csv(gathered_yosys_data_filepath)
         table = pd.DataFrame(columns=columns)
 
         def add_row(instr, arity, template):
-            pass
+            # Row of raw data that we'll convert into an output table row.
+            raw_lakeroad_data_row = lakeroad_data.loc[
+                (
+                    lakeroad_data["module_name"]
+                    == f"lakeroad_lattice_ecp5_{instr}_{arity}"
+                )
+                & (lakeroad_data["template"] == template)
+            ].squeeze()
+            assert raw_lakeroad_data_row.empty or isinstance(
+                raw_lakeroad_data_row, pd.Series
+            )
 
-        raise Exception("not implemented")
+            dsp_impl = lakeroad_data.loc[
+                (
+                    lakeroad_data["module_name"]
+                    == f"lakeroad_lattice_ecp5_{instr}_{arity}"
+                )
+                & (lakeroad_data["template"] == "lattice-ecp5-dsp")
+            ].squeeze()
+            assert dsp_impl.empty or isinstance(dsp_impl, pd.Series)
+
+            raw_yosys_data_row = yosys_data.loc[
+                yosys_data["identifier"] == f"{instr}_{arity}"
+            ].squeeze()
+            print(raw_yosys_data_row)
+            assert isinstance(raw_yosys_data_row, pd.Series)
+
+            raw_diamond_data_row = diamond_data.loc[
+                diamond_data["identifier"] == f"{instr}_{arity}"
+            ].squeeze()
+            assert isinstance(raw_diamond_data_row, pd.Series)
+
+            match template:
+                case "bitwise":
+                    short_template = "BW"
+                case "bitwise-with-carry":
+                    short_template = "Carry"
+                case "multiplication":
+                    short_template = "Mult"
+                case "comparison":
+                    short_template = "Comp"
+                case _:
+                    raise Exception(f"Unknown template: {template}")
+
+            # If we don't actually have an implementation, the template is blank.
+            if raw_lakeroad_data_row.empty:
+                short_template = "--"
+
+            out_row = pd.DataFrame(
+                [
+                    [
+                        instr,
+                        short_template,
+                        raw_lakeroad_data_row["lakeroad_runtime_s"]
+                        if not raw_lakeroad_data_row.empty
+                        else None,
+                        raw_lakeroad_data_row["num_LUT4"]
+                        if not raw_lakeroad_data_row.empty
+                        else None,
+                        raw_lakeroad_data_row["num_CCU2C"]
+                        if not raw_lakeroad_data_row.empty
+                        else None,
+                        None
+                        if dsp_impl.empty
+                        else f"✓({dsp_impl['lakeroad_runtime_s']})",
+                        raw_yosys_data_row["yosys_runtime_s"],
+                        raw_yosys_data_row["LUT4"],
+                        raw_yosys_data_row["L6MUX21"] + raw_yosys_data_row["PFUMX"],
+                        raw_yosys_data_row["CCU2C"],
+                        raw_yosys_data_row["MULT18X18D"],
+                        raw_diamond_data_row["diamond_cpu_time"],
+                        raw_diamond_data_row["num_LUT4"],
+                        raw_diamond_data_row["num_CCU2C"],
+                        raw_diamond_data_row["num_MULT18X18D"]
+                        + raw_diamond_data_row["num_MULT9X9D"],
+                        raw_diamond_data_row["num_ALU54B"],
+                        "X",
+                    ]
+                ],
+                columns=columns,
+            )
+            nonlocal table
+            table = pd.concat([table, out_row], ignore_index=True)
+
+        add_row("and8", 2, "bitwise")
+        add_row("and16", 2, "bitwise")
+        add_row("and32", 2, "bitwise")
+        add_row("or8", 2, "bitwise")
+        add_row("or16", 2, "bitwise")
+        add_row("or32", 2, "bitwise")
+        add_row("xor8", 2, "bitwise")
+        add_row("xor16", 2, "bitwise")
+        add_row("xor32", 2, "bitwise")
+        add_row("not8", 1, "bitwise")
+        add_row("not16", 1, "bitwise")
+        add_row("not32", 1, "bitwise")
+        add_row("mux8", 3, "bitwise")
+        add_row("mux16", 3, "bitwise")
+        add_row("mux32", 3, "bitwise")
+        add_row("add8", 2, "bitwise-with-carry")
+        add_row("add16", 2, "bitwise-with-carry")
+        add_row("add32", 2, "bitwise-with-carry")
+        add_row("sub8", 2, "bitwise-with-carry")
+        add_row("sub16", 2, "bitwise-with-carry")
+        add_row("sub32", 2, "bitwise-with-carry")
+        add_row("mul8", 2, "multiplication")
+        add_row("mul16", 2, "multiplication")
+        add_row("mul32", 2, "multiplication")
+        # table = pd.concat(
+        #    [
+        #        table,
+        #        pd.DataFrame(
+        #            [
+        #                ["mul16", "-", "-", "-", "X", "X"],
+        #                ["mul32", "-", "-", "-", "X", "X"],
+        #            ],
+        #            columns=columns,
+        #        ),
+        #    ],
+        #    ignore_index=True,
+        # )
+        add_row("eq8", 2, "comparison")
+        add_row("eq16", 2, "comparison")
+        add_row("eq32", 2, "comparison")
+        add_row("neq8", 2, "comparison")
+        add_row("neq16", 2, "comparison")
+        add_row("neq32", 2, "comparison")
+        add_row("ugt8", 2, "comparison")
+        add_row("ugt16", 2, "comparison")
+        add_row("ugt32", 2, "comparison")
+        add_row("ult8", 2, "comparison")
+        add_row("ult16", 2, "comparison")
+        add_row("ult32", 2, "comparison")
+        add_row("uge8", 2, "comparison")
+        add_row("uge16", 2, "comparison")
+        add_row("uge32", 2, "comparison")
+        add_row("ule8", 2, "comparison")
+        add_row("ule16", 2, "comparison")
+        add_row("ule32", 2, "comparison")
+
+        table.to_csv(
+            csv_filepath,
+        )
+        styler = table.style.format(precision=1).hide(axis="index")
+        styler.to_latex(tex_filepath)
+        styler.to_excel(excel_filepath)
+
+    return {
+        "actions": [(_impl, [])],
+        "file_dep": [
+            gathered_diamond_data_filepath,
+            gathered_yosys_data_filepath,
+            gathered_lakeroad_data_filepath,
+        ],
+        "targets": [csv_filepath, tex_filepath, excel_filepath],
+    }
