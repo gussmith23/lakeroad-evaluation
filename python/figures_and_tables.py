ae69fa4fb41a1e19e5b2530237d4c6db23c366e3
Gus Smith <guscomps@gmail.com>
2023-01-05 23:11:47 +0000

    Initial (incorrect) version of UltraScale+ table

--- python/figures_and_tables.py
+++ python/figures_and_tables.py
@@ -413,3 +413,302 @@ def task_make_lattice_ecp5_figure(
         ],
         "targets": [csv_filepath, tex_filepath, excel_filepath],
     }
+
+
+@doit.task_params(
+    [
+        {
+            "name": "gathered_lakeroad_data_filepath",
+            "default": str(
+                utils.output_dir()
+                / "gathered_data"
+                / "lakeroad_xilinx_ultrascale_plus_vivado_results.csv"
+            ),
+            "type": str,
+        },
+        {
+            "name": "gathered_vivado_data_filepath",
+            "default": str(
+                utils.output_dir() / "gathered_data" / "vivado_baseline.csv"
+            ),
+            "type": str,
+        },
+        {
+            "name": "gathered_yosys_data_filepath",
+            "default": str(
+                utils.output_dir()
+                / "gathered_data"
+                / "yosys_xilinx_ultrascale_plus_baseline.csv"
+            ),
+            "type": str,
+        },
+        {
+            "name": "csv_filepath",
+            "default": str(
+                utils.output_dir() / "figures" / "xilinx_ultrascale_plus_figure.csv"
+            ),
+            "type": str,
+        },
+        {
+            "name": "tex_filepath",
+            "default": str(
+                utils.output_dir() / "figures" / "xilinx_ultrascale_plus_figure.tex"
+            ),
+            "type": str,
+        },
+        {
+            "name": "excel_filepath",
+            "default": str(
+                utils.output_dir() / "figures" / "xilinx_ultrascale_plus_figure.xlsx"
+            ),
+            "type": str,
+        },
+    ]
+)
+def task_make_xilinx_ultrascale_plus_figure(
+    gathered_lakeroad_data_filepath: Union[str, Path],
+    gathered_vivado_data_filepath: Union[str, Path],
+    gathered_yosys_data_filepath: Union[str, Path],
+    csv_filepath: Union[str, Path],
+    tex_filepath: Union[str, Path],
+    excel_filepath: Union[str, Path],
+):
+    gathered_lakeroad_data_filepath = Path(gathered_lakeroad_data_filepath)
+    gathered_vivado_data_filepath = Path(gathered_vivado_data_filepath)
+    gathered_yosys_data_filepath = Path(gathered_yosys_data_filepath)
+    csv_filepath = Path(csv_filepath)
+    tex_filepath = Path(tex_filepath)
+    excel_filepath = Path(excel_filepath)
+
+    columns = pd.MultiIndex.from_tuples(
+        [
+            ("", "", "Instr"),
+            ("", "", "Template"),
+            ("Xilinx UltraScale+", "Lakeroad", "Runtime in sec"),
+            ("Xilinx UltraScale+", "Lakeroad", "#LUTs"),
+            ("Xilinx UltraScale+", "Lakeroad", "#CARRY8s"),
+            ("Xilinx UltraScale+", "Lakeroad", "DSP? (sec)"),
+            ("Xilinx UltraScale+", "Yosys", "Runtime in sec"),
+            ("Xilinx UltraScale+", "Yosys", "#LUTs"),
+            ("Xilinx UltraScale+", "Yosys", "#muxes"),
+            ("Xilinx UltraScale+", "Yosys", "#CARRY4s"),
+            ("Xilinx UltraScale+", "Yosys", "#INVs"),
+            ("Xilinx UltraScale+", "Yosys", "#DSPs"),
+            ("Xilinx UltraScale+", "Vivado", "Runtime in sec"),
+            ("Xilinx UltraScale+", "Vivado", "#LUTs"),
+            ("Xilinx UltraScale+", "Vivado", "#CARRY8s"),
+            ("Xilinx UltraScale+", "Vivado", "#DSPs"),
+            ("Xilinx UltraScale+", "Diamond", "Support"),
+        ]
+    )
+
+    def _impl():
+        csv_filepath.parent.mkdir(parents=True, exist_ok=True)
+        tex_filepath.parent.mkdir(parents=True, exist_ok=True)
+        excel_filepath.parent.mkdir(parents=True, exist_ok=True)
+
+        lakeroad_data = pd.read_csv(gathered_lakeroad_data_filepath)
+        vivado_data = pd.read_csv(gathered_vivado_data_filepath)
+        yosys_data = pd.read_csv(gathered_yosys_data_filepath)
+        table = pd.DataFrame(columns=columns)
+
+        def add_row(instr, arity, template):
+            # Row of raw data that we'll convert into an output table row.
+            raw_lakeroad_data_row = lakeroad_data.loc[
+                (
+                    lakeroad_data["module_name"]
+                    == f"lakeroad_xilinx_ultrascale_plus_{instr}_{arity}"
+                )
+                & (lakeroad_data["template"] == template)
+            ].squeeze()
+            assert raw_lakeroad_data_row.empty or isinstance(
+                raw_lakeroad_data_row, pd.Series
+            )
+
+            dsp_impl = lakeroad_data.loc[
+                (
+                    lakeroad_data["module_name"]
+                    == f"lakeroad_xilinx_ultrascale_plus_{instr}_{arity}"
+                )
+                & (lakeroad_data["template"] == "xilinx-ultrascale-plus-dsp48e2")
+            ].squeeze()
+            assert dsp_impl.empty or isinstance(dsp_impl, pd.Series)
+
+            raw_yosys_data_row = yosys_data.loc[
+                yosys_data["identifier"] == f"{instr}_{arity}"
+            ].squeeze()
+            assert isinstance(raw_yosys_data_row, pd.Series)
+
+            raw_vivado_data_row = vivado_data.loc[
+                vivado_data["identifier"] == f"{instr}_{arity}"
+            ].squeeze()
+            assert isinstance(raw_vivado_data_row, pd.Series)
+
+            match template:
+                case "bitwise":
+                    short_template = "BW"
+                case "bitwise-with-carry":
+                    short_template = "Carry"
+                case "multiplication":
+                    short_template = "Mult"
+                case "comparison":
+                    short_template = "Comp"
+                case _:
+                    raise Exception(f"Unknown template: {template}")
+
+            # If we don't actually have an implementation, the template is blank.
+            if raw_lakeroad_data_row.empty:
+                short_template = "--"
+
+            out_row = pd.DataFrame(
+                [
+                    [
+                        instr,
+                        short_template,
+                        raw_lakeroad_data_row["lakeroad_runtime_s"]
+                        if not raw_lakeroad_data_row.empty
+                        else None,
+                        # LUTs
+                        (
+                            (
+                                raw_lakeroad_data_row["LUT2"]
+                                if "LUT2" in raw_lakeroad_data_row
+                                else 0
+                            )
+                            + (
+                                raw_lakeroad_data_row["LUT3"]
+                                if "LUT3" in raw_lakeroad_data_row
+                                else 0
+                            )
+                            + (
+                                raw_lakeroad_data_row["LUT4"]
+                                if "LUT4" in raw_lakeroad_data_row
+                                else 0
+                            )
+                            + (
+                                raw_lakeroad_data_row["LUT5"]
+                                if "LUT5" in raw_lakeroad_data_row
+                                else 0
+                            )
+                            + (
+                                raw_lakeroad_data_row["LUT6"]
+                                if "LUT6" in raw_lakeroad_data_row
+                                else 0
+                            )
+                            + (
+                                raw_lakeroad_data_row["LUT6_2"]
+                                if "LUT6_2" in raw_lakeroad_data_row
+                                else 0
+                            )
+                        )
+                        if not raw_lakeroad_data_row.empty
+                        else None,
+                        # Carries
+                        (
+                            (
+                                raw_lakeroad_data_row["CARRY4"]
+                                if "CARRY4" in raw_lakeroad_data_row
+                                else 0
+                            )
+                            + (
+                                raw_lakeroad_data_row["CARRY8"]
+                                if "CARRY8" in raw_lakeroad_data_row
+                                else 0
+                            )
+                        )
+                        if not raw_lakeroad_data_row.empty
+                        else None,
+                        None
+                        if dsp_impl.empty
+                        else f"✓({dsp_impl['lakeroad_runtime_s']})",
+                        raw_yosys_data_row["yosys_runtime_s"],
+                        raw_yosys_data_row["LUT4"],
+                        # Yosys muxes
+                        raw_yosys_data_row["MUXF7"] + raw_yosys_data_row["MUXF8"],
+                        raw_yosys_data_row["CARRY4"],
+                        raw_yosys_data_row["INV"],
+                        raw_yosys_data_row["DSP48E2"],
+                        raw_vivado_data_row["synth_time"],
+                        raw_vivado_data_row["clb_luts"],
+                        raw_vivado_data_row["carry8s"],
+                        "TODO",
+                        "X",
+                    ]
+                ],
+                columns=columns,
+            )
+            nonlocal table
+            table = pd.concat([table, out_row], ignore_index=True)
+
+        add_row("and8", 2, "bitwise")
+        add_row("and16", 2, "bitwise")
+        add_row("and32", 2, "bitwise")
+        add_row("or8", 2, "bitwise")
+        add_row("or16", 2, "bitwise")
+        add_row("or32", 2, "bitwise")
+        add_row("xor8", 2, "bitwise")
+        add_row("xor16", 2, "bitwise")
+        add_row("xor32", 2, "bitwise")
+        add_row("not8", 1, "bitwise")
+        add_row("not16", 1, "bitwise")
+        add_row("not32", 1, "bitwise")
+        add_row("mux8", 3, "bitwise")
+        add_row("mux16", 3, "bitwise")
+        add_row("mux32", 3, "bitwise")
+        add_row("add8", 2, "bitwise-with-carry")
+        add_row("add16", 2, "bitwise-with-carry")
+        add_row("add32", 2, "bitwise-with-carry")
+        add_row("sub8", 2, "bitwise-with-carry")
+        add_row("sub16", 2, "bitwise-with-carry")
+        add_row("sub32", 2, "bitwise-with-carry")
+        add_row("mul8", 2, "multiplication")
+        add_row("mul16", 2, "multiplication")
+        add_row("mul32", 2, "multiplication")
+        # table = pd.concat(
+        #    [
+        #        table,
+        #        pd.DataFrame(
+        #            [
+        #                ["mul16", "-", "-", "-", "X", "X"],
+        #                ["mul32", "-", "-", "-", "X", "X"],
+        #            ],
+        #            columns=columns,
+        #        ),
+        #    ],
+        #    ignore_index=True,
+        # )
+        add_row("eq8", 2, "comparison")
+        add_row("eq16", 2, "comparison")
+        add_row("eq32", 2, "comparison")
+        add_row("neq8", 2, "comparison")
+        add_row("neq16", 2, "comparison")
+        add_row("neq32", 2, "comparison")
+        add_row("ugt8", 2, "comparison")
+        add_row("ugt16", 2, "comparison")
+        add_row("ugt32", 2, "comparison")
+        add_row("ult8", 2, "comparison")
+        add_row("ult16", 2, "comparison")
+        add_row("ult32", 2, "comparison")
+        add_row("uge8", 2, "comparison")
+        add_row("uge16", 2, "comparison")
+        add_row("uge32", 2, "comparison")
+        add_row("ule8", 2, "comparison")
+        add_row("ule16", 2, "comparison")
+        add_row("ule32", 2, "comparison")
+
+        table.to_csv(
+            csv_filepath,
+        )
+        styler = table.style.format(precision=1).hide(axis="index")
+        styler.to_latex(tex_filepath)
+        styler.to_excel(excel_filepath)
+
+    return {
+        "actions": [(_impl, [])],
+        "file_dep": [
+            gathered_vivado_data_filepath,
+            gathered_yosys_data_filepath,
+            gathered_lakeroad_data_filepath,
+        ],
+        "targets": [csv_filepath, tex_filepath, excel_filepath],
+    }
