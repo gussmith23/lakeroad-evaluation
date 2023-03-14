09c32e0067d8638185cf3ce908476cbaeed1e1d6
Gus Smith <guscomps@gmail.com>
2023-03-14 23:08:09 +0000

    Add list of files and download script

---
+++ dsp-annotated-benchmarks/submodules/download_files.py
@@ -0,0 +1,12 @@
+import yaml
+import subprocess
+
+def main():
+    with open("files.yml") as f:
+        files = yaml.safe_load(f)
+    
+    for file in files:
+        subprocess.run(['wget', '-o', file['dest_filename'], file['url']], check=True)
+
+if __name__=="__main__":
+    main()
