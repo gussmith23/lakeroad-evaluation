65ddddc698aa50bba6edc42f7331201a5f3a4e1b
Gus Smith <guscomps@gmail.com>
2023-03-15 00:49:10 +0000

    new files

---
+++ dsp-annotated-benchmarks/submodules/adamgallas_fpga_accelerator_yolov3tiny_vivado_prj_source_cal_mul_int8_x2_dsp.v
@@ -0,0 +1,14 @@
+--2023-03-15 00:47:27--  https://github.com/adamgallas/fpga_accelerator_yolov3tiny/blob/15963fa5510271e66835a7c59448beebf8bc2d38/vivado_prj/source/cal_mul_int8_x2_dsp.v
+Resolving github.com (github.com)... 192.30.255.112
+Connecting to github.com (github.com)|192.30.255.112|:443... connected.
+HTTP request sent, awaiting response... 200 OK
+Length: unspecified [text/html]
+Saving to: ‘cal_mul_int8_x2_dsp.v’
+
+     0K .......... .......... .......... .......... .......... 13.6M
+    50K .......... .......... .......... .......... .......... 19.7M
+   100K .......... .......... .......... .......... .......... 20.7M
+   150K ......                                                  108M=0.008s
+
+2023-03-15 00:47:27 (18.0 MB/s) - ‘cal_mul_int8_x2_dsp.v’ saved [159967]
+
--- dsp-annotated-benchmarks/submodules/muzicarski_bachelor_DESKEW_IP_V1_2_src_dim_mult.v
+++ dsp-annotated-benchmarks/submodules/muzicarski_bachelor_DESKEW_IP_V1_2_src_dim_mult.v
@@ -1,14 +1,14 @@
---2023-03-14 23:06:24--  https://github.com/muzicarski/bachelor/blob/2d477a54404cdaf9855af1d46934b3c8f0c3d805/DESKEW_IP_V1_2/src/dim_mult.v
+--2023-03-15 00:47:26--  https://github.com/muzicarski/bachelor/blob/2d477a54404cdaf9855af1d46934b3c8f0c3d805/DESKEW_IP_V1_2/src/dim_mult.v
 Resolving github.com (github.com)... 192.30.255.112
 Connecting to github.com (github.com)|192.30.255.112|:443... connected.
 HTTP request sent, awaiting response... 200 OK
 Length: unspecified [text/html]
-Saving to: ‘dim_mult.v.1’
+Saving to: ‘dim_mult.v’
 
-     0K .......... .......... .......... .......... .......... 15.8M
-    50K .......... .......... .......... .......... .......... 20.8M
-   100K .......... .......... .......... .......... .......... 22.8M
-   150K .......... ....                                        34.5M=0.008s
+     0K .......... .......... .......... .......... .......... 17.3M
+    50K .......... .......... .......... .......... .......... 12.5M
+   100K .......... .......... .......... .......... .......... 33.6M
+   150K .......... ....                                        15.4M=0.009s
 
-2023-03-14 23:06:25 (20.1 MB/s) - ‘dim_mult.v.1’ saved [168198]
+2023-03-15 00:47:27 (17.6 MB/s) - ‘dim_mult.v’ saved [168204]
 
