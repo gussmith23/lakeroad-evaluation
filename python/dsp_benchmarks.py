0664490f17a82e30df09282059d6caec0c39600a
Gus Smith <guscomps@gmail.com>
2023-04-16 23:01:24 +0000

    Make Verilator task for Lattice

--- python/dsp_benchmarks.py
+++ python/dsp_benchmarks.py
@@ -108,6 +108,30 @@ def task_dsp_benchmarks():
             reset_name=benchmark["reset_name"] if "reset_name" in benchmark else None,
         )
 
+        yield verilator.make_verilator_task(
+            name=f"simulate_{filepath.stem}_lakeroad_lattice_ecp5_iter{iter}",
+            obj_dir_dir=(lakeroad_lattice_ecp5_base_filepath / "verilator_obj_dirs"),
+            test_module_filepath=(lakeroad_lattice_ecp5_base_filepath / filepath.name),
+            ground_truth_module_filepath=benchmark["filepath"],
+            include_dirs=benchmark["verilator_include_dirs"],
+            extra_args=benchmark["extra_verilator_args"],
+            module_inputs=[(name, int(bw)) for [name, bw] in benchmark["inputs"]],
+            testbench_cc_filepath=lakeroad_lattice_ecp5_base_filepath / "testbench.cc",
+            testbench_exe_filepath=lakeroad_lattice_ecp5_base_filepath / "testbench",
+            testbench_inputs_filepath=lakeroad_lattice_ecp5_base_filepath
+            / "testbench_inputs.txt",
+            testbench_stdout_log_filepath=(
+                lakeroad_lattice_ecp5_base_filepath / "testbench.stdout"
+            ),
+            testbench_stderr_log_filepath=(
+                lakeroad_lattice_ecp5_base_filepath / "testbench.stderr"
+            ),
+            makefile_filepath=(lakeroad_lattice_ecp5_base_filepath / "Makefile"),
+            clock_name=benchmark["clock_name"],
+            initiation_interval=benchmark["initiation_interval"],
+            output_signal=benchmark["outputs"][0][0],
+        )
+
         yield hardware_compilation.make_xilinx_ultrascale_plus_yosys_synthesis_task(
             input_filepath=filepath,
             output_dirpath=base_filepath
