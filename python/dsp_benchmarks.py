46be2b085038b125c1418b320e5310568689cd8e
Gus Smith <guscomps@gmail.com>
2023-04-17 03:17:31 +0000

    Add synthesis to Intel via Lakeroad

--- python/dsp_benchmarks.py
+++ python/dsp_benchmarks.py
@@ -132,6 +132,58 @@ def task_dsp_benchmarks():
             output_signal=benchmark["outputs"][0][0],
         )
 
+        # Intel
+        lakeroad_intel_base_filepath = (
+            base_filepath / "lakeroad_intel" / f"iter{iter}" / filepath.stem
+        )
+        yield make_lakeroad_task(
+            template="dsp",
+            out_module_name="out",
+            out_filepath=lakeroad_intel_base_filepath / filepath.name,
+            architecture="intel",
+            time_filepath=(
+                lakeroad_intel_base_filepath / filepath.with_suffix(".time").name
+            ),
+            json_filepath=(
+                lakeroad_intel_base_filepath / filepath.with_suffix(".json").name
+            ),
+            verilog_module_filepath=benchmark["filepath"],
+            top_module_name=benchmark["module_name"],
+            verilog_module_out_signal=(
+                benchmark["outputs"][0][0],
+                benchmark["outputs"][0][1],
+            ),
+            name=f"{filepath.stem}_lakeroad_intel_iter{iter}",
+            initiation_interval=benchmark["initiation_interval"],
+            inputs=[(name, int(bw)) for [name, bw] in benchmark["inputs"]],
+            clock_name=benchmark["clock_name"],
+            reset_name=benchmark["reset_name"] if "reset_name" in benchmark else None,
+        )
+        yield verilator.make_verilator_task(
+            name=f"simulate_{filepath.stem}_lakeroad_intel_iter{iter}",
+            obj_dir_dir=(lakeroad_intel_base_filepath / "verilator_obj_dirs"),
+            test_module_filepath=(lakeroad_intel_base_filepath / filepath.name),
+            ground_truth_module_filepath=benchmark["filepath"],
+            include_dirs=benchmark["verilator_include_dirs"]["intel"],
+            extra_args=benchmark["extra_verilator_args"]["intel"],
+            module_inputs=[(name, int(bw)) for [name, bw] in benchmark["inputs"]],
+            testbench_cc_filepath=lakeroad_intel_base_filepath / "testbench.cc",
+            testbench_exe_filepath=lakeroad_intel_base_filepath / "testbench",
+            testbench_inputs_filepath=(
+                lakeroad_intel_base_filepath / "testbench_inputs.txt"
+            ),
+            testbench_stdout_log_filepath=(
+                lakeroad_intel_base_filepath / "testbench.stdout"
+            ),
+            testbench_stderr_log_filepath=(
+                lakeroad_intel_base_filepath / "testbench.stderr"
+            ),
+            makefile_filepath=lakeroad_intel_base_filepath / "Makefile",
+            clock_name=benchmark["clock_name"],
+            initiation_interval=benchmark["initiation_interval"],
+            output_signal=benchmark["outputs"][0][0],
+        )
+
         yield hardware_compilation.make_xilinx_ultrascale_plus_yosys_synthesis_task(
             input_filepath=filepath,
             output_dirpath=base_filepath
