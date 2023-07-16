3db4b206c2293f80b74e7fbd8c16fd514e02e0c9
Vishal Canumalla <vishalc@cs.washington.edu>
2023-07-16 01:25:54 +0000

    initial cleanup of experiment tasks. Still TODOs

--- python/robustness_experiments.py
+++ python/robustness_experiments.py
@@ -168,36 +168,36 @@ def task_robustness_experiments():
             ],
             "file_dep": [base_path / "collected_data.json"],
         }
-        # yield verilator.make_verilator_task(
-        #     f"{experiment['module_name']}:lakeroad:verilator",
-        #     obj_dir_dir=base_path / "verilator_obj_dir",
-        #     test_module_filepath=base_path / "output.v",
-        #     ground_truth_module_filepath=experiment["filepath"],
-        #     module_inputs=experiment["inputs"],
-        #     clock_name="clk",
-        #     initiation_interval=experiment["stages"],
-        #     testbench_cc_filepath=base_path / "testbench.cc",
-        #     testbench_exe_filepath=base_path / "testbench",
-        #     testbench_inputs_filepath=base_path / "testbench_inputs.txt",
-        #     testbench_stdout_log_filepath=base_path / "testbench_stdout.log",
-        #     testbench_stderr_log_filepath=base_path / "testbench_stderr.log",
-        #     makefile_filepath=base_path / "Makefile",
-        #     output_signal="out",
-        #     include_dirs=[
-        #         "/home/acheung8/lakeroad-evaluation/lakeroad-private/DSP48E2"
-        #     ],
-        #     extra_args=[
-        #         "-DXIL_XECLIB",
-        #         "-Wno-UNOPTFLAT",
-        #         "-Wno-LATCH",
-        #         "-Wno-WIDTH",
-        #         "-Wno-STMTDLY",
-        #         "-Wno-CASEX",
-        #         "-Wno-TIMESCALEMOD",
-        #         "-Wno-PINMISSING",
-        #     ],
-        #     max_num_tests=10000,
-        # )
+        yield verilator.make_verilator_task(
+            f"{experiment['module_name']}:lakeroad:verilator",
+            obj_dir_dir=base_path / "verilator_obj_dir",
+            test_module_filepath=base_path / "output.v",
+            ground_truth_module_filepath=experiment["filepath"],
+            module_inputs=experiment["inputs"],
+            clock_name="clk",
+            initiation_interval=experiment["stages"],
+            testbench_cc_filepath=base_path / "testbench.cc",
+            testbench_exe_filepath=base_path / "testbench",
+            testbench_inputs_filepath=base_path / "testbench_inputs.txt",
+            testbench_stdout_log_filepath=base_path / "testbench_stdout.log",
+            testbench_stderr_log_filepath=base_path / "testbench_stderr.log",
+            makefile_filepath=base_path / "Makefile",
+            output_signal="out",
+            include_dirs=[
+                "/home/acheung8/lakeroad-evaluation/lakeroad-private/DSP48E2"
+            ],
+            extra_args=[
+                "-DXIL_XECLIB",
+                "-Wno-UNOPTFLAT",
+                "-Wno-LATCH",
+                "-Wno-WIDTH",
+                "-Wno-STMTDLY",
+                "-Wno-CASEX",
+                "-Wno-TIMESCALEMOD",
+                "-Wno-PINMISSING",
+            ],
+            max_num_tests=10000,
+        )
 
         # # diamond-lattice, lakeroad-lattice, yosys-lattice
         if "diamond" in experiment["tool"]:
@@ -290,6 +290,17 @@ def task_robustness_experiments():
             / experiment["module_name"]
             / "quartus_intel"
         )
+        yield quartus.make_quartus_task(
+            identifier = experiment["module_name"],
+            top_module_name = experiment["module_name"],
+            source_input_filepath = experiment["filepath"],
+            summary_output_filepath = base_path / "summary.map.summary",
+            json_output_filepath = base_path / "out.json",
+            time_output_filepath = base_path / "out.time",
+            collected_data_output_filepath = base_path / "collected_data.json",
+            iteration = 0,
+            task_name = f"{experiment['module_name']}:quartus_intel"
+        )
 
 
 #         def make_lattice_ecp5_yosys_synthesis_task(
@@ -300,7 +311,273 @@ def task_robustness_experiments():
 #     name: Optional[str] = None,
 #     collect_args: Optional[Dict[str, Any]] = None,
 # ):
+def task_robustness_experiments():
+    """Robustness experiments: finding Verilog files that existing tools can't map"""
 
+    entries = yaml.safe_load(stream=open("robustness-manifest.yml", "r"))
+    # each entry process the workloads that go along with it
+    for entry in entries:
+        backends = entry["backends"]
+        if "xilinx" in backends:
+
+            # base path for vivado tasks for this entry
+            base_path = (
+                utils.output_dir()
+                / "robustness_experiments"
+                / entry["module_name"]
+                / "vivado"
+            )
+            # vivado synthesis
+            yield hardware_compilation.make_xilinx_ultrascale_plus_vivado_synthesis_task_opt(
+                input_filepath=entry["filepath"],
+                output_dirpath=base_path,
+                module_name=entry["module_name"],
+                # TODO(@gussmith23): Hardcoding clock name and period here.
+                # 0.5ns chosen after a few iterations.
+                clock_info=("clk", 0.5, (0.0, 0.25)),
+                name=f"{entry['module_name']}:vivado",
+                # Makes Vivado try harder to put things on DSPs.
+                directive="AreaMultThresholdDSP",
+                # If our timing constraints are aggressive enough, it won't meet
+                # timing. This is okay; don't fail. We want aggressive
+                # constraints so we know Vivado is trying as hard as it can.
+                fail_if_constraints_not_met=False,
+            )
+            yield {
+                "name": f"{entry['module_name']}:vivado:dsp_check",
+                "actions": [
+                    (
+                        check_for_dsp,
+                        [],
+                        {
+                            "resource_utilization_json_filepath": (
+                                base_path / f"{Path(entry['filepath']).stem}_resource_utilization.json"
+                            ),
+                            "module_name": entry["module_name"],
+                        },
+                    )
+                ],
+                "file_dep": [(base_path / f"{Path(entry['filepath']).stem}_resource_utilization.json")],
+            }
+
+            # Lakeroad Synthesis for xilinx backend
+            base_path = (
+            utils.output_dir()
+            / "robustness_experiments"
+            / entry["module_name"]
+            / "lakeroad_xilinx_ultrascale_plus"
+            )
+
+            yield lakeroad.make_lakeroad_task(
+                # TODO: correct?
+                iteration=0,
+                identifier=entry["module_name"],
+                collected_data_output_filepath=base_path / "collected_data.json",
+                template="dsp",
+                out_module_name="output",
+                out_filepath=base_path / "output.v",
+                architecture="xilinx-ultrascale-plus",
+                time_filepath=base_path / "out.time",
+                json_filepath=base_path / "out.json",
+                verilog_module_filepath=entry["filepath"],
+                top_module_name=entry["module_name"],
+                clock_name="clk",
+                name=entry["module_name"] + ":lakeroad-xilinx",
+                initiation_interval=entry["stages"],
+                inputs=entry["inputs"],
+                verilog_module_out_signal=("out", entry["bitwidth"]),
+            )
+            yield {
+                "name": f"{entry['module_name']}:lakeroad-xilinx:dsp_check",
+                "actions": [
+                    (
+                        check_for_dsp,
+                        [],
+                        {
+                            "resource_utilization_json_filepath": base_path
+                            / "collected_data.json",
+                            "module_name": entry["module_name"],
+                        },
+                    )
+                ],
+                "file_dep": [base_path / "collected_data.json"],
+            }
+            yield verilator.make_verilator_task(
+                f"{entry['module_name']}:lakeroad:verilator",
+                obj_dir_dir=base_path / "verilator_obj_dir",
+                test_module_filepath=base_path / "output.v",
+                ground_truth_module_filepath=entry["filepath"],
+                module_inputs=entry["inputs"],
+                clock_name="clk",
+                initiation_interval=entry["stages"],
+                testbench_cc_filepath=base_path / "testbench.cc",
+                testbench_exe_filepath=base_path / "testbench",
+                testbench_inputs_filepath=base_path / "testbench_inputs.txt",
+                testbench_stdout_log_filepath=base_path / "testbench_stdout.log",
+                testbench_stderr_log_filepath=base_path / "testbench_stderr.log",
+                makefile_filepath=base_path / "Makefile",
+                output_signal="out",
+                include_dirs=[
+                    "/home/acheung8/lakeroad-evaluation/lakeroad-private/DSP48E2"
+                ],
+                extra_args=[
+                    "-DXIL_XECLIB",
+                    "-Wno-UNOPTFLAT",
+                    "-Wno-LATCH",
+                    "-Wno-WIDTH",
+                    "-Wno-STMTDLY",
+                    "-Wno-CASEX",
+                    "-Wno-TIMESCALEMOD",
+                    "-Wno-PINMISSING",
+                ],
+                max_num_tests=10000,
+            )
+            # yosys synthesis for xilinx backend
+            base_path = (
+                utils.output_dir()
+                / "robustness_experiments"
+                / entry["module_name"]
+                / "yosys_xilinx_ultrascale_plus"
+            )
+            task = (
+                hardware_compilation.make_xilinx_ultrascale_plus_yosys_synthesis_task(
+                    input_filepath=entry["filepath"],
+                    output_dirpath=base_path,
+                    module_name=entry["module_name"],
+                )
+            )
+            task["name"] = f"{entry['module_name']}:yosys_xilinx_ultrascale_plus"
+            yield task
+
+            resources_filepath = (
+                base_path / f"{entry['module_name']}.resources.json"
+            )
+            yield {
+                "name": f"{entry['module_name']}:yosys_xilinx:dsp_check",
+                "actions": [
+                    (
+                        check_for_dsp,
+                        [],
+                        {
+                            "resource_utilization_json_filepath": resources_filepath,
+                            "module_name": entry["module_name"],
+                        },
+                    )
+                ],
+                "file_dep": [resources_filepath],
+            }
+
+        if "lattice" in backends:
+            # diamond-lattice, lakeroad-lattice, yosys-lattice
+            base_path = (
+                utils.output_dir()
+                / "robustness_experiments"
+                / entry["module_name"]
+                / "diamond"
+            )
+            yield hardware_compilation.make_lattice_ecp5_diamond_synthesis_task(
+                input_filepath=entry["filepath"],
+                output_dirpath=base_path,
+                module_name=entry["module_name"],
+                name=f"{entry['module_name']}:diamond"
+            )
+            # TODO(@vcanumalla): ADD lattice backend dsp check
+            # yield {
+            #     "name": f"{entry['module_name']}:diamond:dsp_check",
+            #     "actions": [
+            #         (
+            #             check_for_dsp,
+            #             [],
+            #             {
+            #                 "resource_utilization_json_filepath": base_path
+            #                 / "test_resources.json",
+            #                 "module_name": entry["module_name"],
+            #             },
+            #         )
+            #     ],
+            #     "file_dep": [base_path / "test_resources.json"],
+            # }
+
+            base_path = (
+                utils.output_dir()
+                / "robustness_experiments"
+                / entry["module_name"]
+                / "lakeroad_lattice_ecp5"
+            )
+            task = lakeroad.make_lakeroad_task(
+                iteration=0,
+                identifier=entry["module_name"],
+                collected_data_output_filepath=base_path / "collected_data.json",
+                template="dsp",
+                out_module_name="output",
+                out_filepath=base_path / "output.v",
+                architecture="lattice-ecp5",
+                time_filepath=base_path / "out.time",
+                json_filepath=base_path / "out.json",
+                verilog_module_filepath=entry["filepath"],
+                top_module_name=entry["module_name"],
+                clock_name="clk",
+                name=entry["module_name"] + ":lakeroad-lattice",
+                initiation_interval=entry["stages"],
+                inputs=entry["inputs"],
+                verilog_module_out_signal=("out", entry["bitwidth"]),
+            )
+            yield task
+
+            # TODO(@vcanumalla): ADD dsp check and verilator sim
+
+            # yosys for diamond backend
+            yield hardware_compilation.make_lattice_ecp5_yosys_synthesis_task(
+                input_filepath=entry["filepath"],
+                output_dirpath=(
+                    utils.output_dir()
+                    / "robustness_experiments"
+                    / entry["module_name"]
+                    / "yosys_lattice_ecp5"
+                ),
+                module_name=entry["module_name"],
+                name=f"{entry['module_name']}:yosys_lattice_ecp5",
+            )
+            resources_filepath = (
+                base_path / f"{entry['module_name']}.resources.json"
+            )
+
+            yield {
+                "name": f"{entry['module_name']}:yosys_lattice_ecp5:dsp_check",
+                "actions": [
+                    (
+                        check_for_dsp,
+                        [],
+                        {
+                            "resource_utilization_json_filepath": resources_filepath,
+                            "module_name": entry["module_name"],
+                        },
+                    )
+                ],
+                "file_dep": [resources_filepath],
+            }
+
+        if "intel" in backends:
+            base_path = (
+                utils.output_dir()
+                / "robustness_experiments"
+                / entry["module_name"]
+                / "quartus_intel"
+            )
+            yield quartus.make_quartus_task(
+                identifier = entry["module_name"],
+                top_module_name = entry["module_name"],
+                source_input_filepath = entry["filepath"],
+                summary_output_filepath = base_path / "summary.map.summary",
+                json_output_filepath = base_path / "out.json",
+                time_output_filepath = base_path / "out.time",
+                collected_data_output_filepath = base_path / "collected_data.json",
+                iteration = 0,
+                task_name = f"{entry['module_name']}:quartus_intel"
+            )
+
+            # TODO(@vcanumalla): Add rest of quartus (lakeroad, yosys)
+            
 
 if __name__ == "__main__":
     print(create_manifest())
