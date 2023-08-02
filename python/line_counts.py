8c149bc3a529c7f648602ebcce8c3b9e41f727c9
Gus Smith <guscomps@gmail.com>
2023-08-02 10:56:45 -0700

    Add line counts for Verilog files

--- python/line_counts.py
+++ python/line_counts.py
@@ -1,3 +1,4 @@
+import sys
 import pandas as pd
 import utils
 import subprocess
@@ -7,6 +8,9 @@ import json
 def _run_cloc(filepath, file_type: str):
     """Run cloc, return lines of code"""
 
+    # cloc doesn't actually fail on failure...not handling that case for now.
+    # This captures a lot of cases where cloc would fail.
+    assert filepath.exists()
     p = subprocess.run(["cloc", filepath, "--json"], check=True, capture_output=True)
 
     results = json.loads(p.stdout)
@@ -63,4 +67,92 @@ def task_architecture_descriptions_line_counts():
         "actions": [(_impl, [output_csv_filepath])],
         "targets": [output_csv_filepath],
         "file_dep": [lattice_file, xilinx_file, intel_file, sofa_file],
+    }
+
+
+def task_verilog_file_line_counts():
+    filepaths = [
+        utils.lakeroad_evaluation_dir()
+        / "lakeroad"
+        / "modules_for_importing"
+        / "SOFA"
+        / "frac_lut4.v",
+        utils.lakeroad_evaluation_dir()
+        / "lakeroad"
+        / "modules_for_importing"
+        / "lattice_ecp5"
+        / "CCU2C.v",
+        utils.lakeroad_evaluation_dir()
+        / "lakeroad"
+        / "modules_for_importing"
+        / "lattice_ecp5"
+        / "LUT2.v",
+        utils.lakeroad_evaluation_dir()
+        / "lakeroad"
+        / "modules_for_importing"
+        / "lattice_ecp5"
+        / "LUT4.v",
+        utils.lakeroad_evaluation_dir()
+        / "lakeroad"
+        / "modules_for_importing"
+        / "xilinx_ultrascale_plus"
+        / "CARRY8.v",
+        utils.lakeroad_evaluation_dir()
+        / "lakeroad"
+        / "modules_for_importing"
+        / "xilinx_ultrascale_plus"
+        / "DSP48E2.v",
+        utils.lakeroad_evaluation_dir()
+        / "lakeroad"
+        / "modules_for_importing"
+        / "xilinx_ultrascale_plus"
+        / "LUT6.v",
+        utils.lakeroad_evaluation_dir()
+        / "lakeroad"
+        / "modules_for_importing"
+        / "xilinx_ultrascale_plus"
+        / "LUT6_2.v",
+        utils.lakeroad_evaluation_dir()
+        / "lakeroad-private"
+        / "altera_mf"
+        / "altera_mf_modified.v",
+        utils.lakeroad_evaluation_dir()
+        / "lakeroad-private"
+        / "lattice_ecp5"
+        / "modified_ALU24B.v",
+        utils.lakeroad_evaluation_dir()
+        / "lakeroad-private"
+        / "lattice_ecp5"
+        / "modified_ALU54B.v",
+        utils.lakeroad_evaluation_dir()
+        / "lakeroad-private"
+        / "lattice_ecp5"
+        / "ALU54A_modified_for_racket_import.v",
+        utils.lakeroad_evaluation_dir()
+        / "lakeroad-private"
+        / "lattice_ecp5"
+        / "MULT18X18C_modified_for_racket_import.v",
+        utils.lakeroad_evaluation_dir()
+        / "lakeroad-private"
+        / "lattice_ecp5"
+        / "MULT18X18D_modified_for_racket_import.v",
+    ]
+
+    def _impl(filepaths, output_csv_filepath):
+        output_csv_filepath.parent.mkdir(parents=True, exist_ok=True)
+        pd.DataFrame.from_records(
+            [
+                (
+                    str(f.relative_to(utils.lakeroad_evaluation_dir())),
+                    _run_cloc(f, "Verilog-SystemVerilog"),
+                )
+                for f in filepaths
+            ]
+        ).to_csv(output_csv_filepath, index=False)
+
+    output_csv_filepath = utils.output_dir() / "line_counts" / "imported_verilog.csv"
+    return {
+        "actions": [(_impl, [filepaths, output_csv_filepath])],
+        "targets": [output_csv_filepath],
+        "file_dep": filepaths,
     }
