c2c93e0440c9333820114fc2471c88e446aecd8a
Gus Smith <guscomps@gmail.com>
2024-01-20 14:39:24 -0800

    Print uptime at an interval

--- run-evaluation.sh
+++ run-evaluation.sh
@@ -17,6 +17,12 @@
 #   tasks. Suggestion: nproc - buffer
 # "buffer" is a number of cores to leave free for the OS and other tasks. I
 # suggest about 10% of cores.
+#
+# You can also optionally set PRINT_UPTIME_INTERVAL to an interval (in seconds)
+# at which to print the uptime. This is useful for performance debugging,
+# to understand which parts of the evaluation are using the most resources.
+
+PRINT_UPTIME_INTERVAL="${PRINT_UPTIME_INTERVAL:-0}"
 
 if [[ -z "$NUM_JOBS_VIVADO_TASKS" ]]; then
     echo "Must provide NUM_JOBS_VIVADO_TASKS in environment" 1>&2
@@ -31,6 +37,24 @@ fi
 if [[ -z "$NUM_JOBS_OTHER_TASKS" ]]; then
     echo "Must provide NUM_JOBS_OTHER_TASKS in environment" 1>&2
     exit 1
+fi
+
+## Optionally track uptime.
+#
+# Cleanup function to kill the uptime process.
+cleanup() {
+  if [[ -n "$UPTIME_PID" ]]; then
+    kill $UPTIME_PID
+  fi
+}
+trap cleanup EXIT
+# Start the uptime process.
+UPTIME_PID=""
+if [[ -n "$PRINT_UPTIME_INTERVAL" ]] && [[ $PRINT_UPTIME_INTERVAL -gt 0 ]]; then
+  while true; do
+  uptime; sleep 1
+  done &
+  UPTIME_PID=$!
 fi
 
 doit --continue -n $NUM_JOBS_VIVADO_TASKS \
